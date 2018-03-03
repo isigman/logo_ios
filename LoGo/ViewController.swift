5c5bea318a76f72c4d9ea62ffad31b133b75eccf
Ira Sigman <irasigman@gmail.com>
2018-03-03 01:03:52 -0500

    Initial Commit

---
+++ LoGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoGo
+//
+//  Created by Ira Sigman on 3/3/18.
+//  Copyright © 2018 Ira Sigman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
